baed5d9903568293c984a030c7fc5ca9b5e56061
Ayushi <miss.ayushigaurav01@gmail.com>
2021-02-12 15:59:20 +0530

    my third commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Para from './para';
+import List from './List';
+
+ function App() {
+    return(
+ <>
+  
+   <Para/>
+   <List/>
+ </>
+ );
+}
+
+export default App;
---
+++ src/List.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function List(){
+ return (
+<ol>
+   <li>Components Reusability</li>
+   <li>Tree Reconciliation in Action</li>
+   <li>ECMAScript and TC39</li>
+   <li>Variables and Block Scopes</li>
+   <li>Arrow Functions</li>
+   <li>Object Literals</li>
+ </ol>
+    );
+}
+
+export default List;
